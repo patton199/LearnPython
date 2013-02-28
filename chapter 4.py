a4920b6e32fd6f138c57db940e539d99b4cbc799
patton199 <patton199@163.com>
2013-02-28 18:09:33 +0800

    chapter 4 add

---
+++ chapter 4.py
@@ -0,0 +1,23 @@
+apples = ["Mackintosh", "Golden Delicious", "Fuji", "Mitsu"]
+apple_trees = ["Golden Delicious", "Fuji", "Mitsu", "Mackintosh"]
+print(apples == apple_trees)
+# result false
+
+apple_trees = ["Mackintosh", "Golden Delicious", "Fuji", "Mitsu"]
+print(apples == apple_trees)
+# result True
+
+tuesday_breakfast_sold = {"pancakes": 10, "french toast": 4, "bagels": 32, "omelets": 12, "egg and sausages": 13}
+wednesday_breakfast_sold = {"pancakes": 8, "french toast": 5, "bagels": 22, "omelets": 16, "egg and sausages": 22}
+print(tuesday_breakfast_sold == wednesday_breakfast_sold)
+# result false
+
+wednesday_breakfast_sold = {"pancakes": 10, "french toast": 4, "bagels": 32, "omelets": 12, "egg and sausages": 13}
+print(tuesday_breakfast_sold == wednesday_breakfast_sold)
+# result true
+
+'''
+if :
+elif :
+	
+'''
